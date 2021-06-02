e1424ccc6c936b07203fd92cbd3506df62a6f425
38063 <xeilen1112@gmail.com>
2021-06-02 22:28:17 +0300

    first commit

---
+++ src/views/Task.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="task-view">
+    <div class="flex flex-col flex-grow item-start justify-between px-4">
+      {{ task }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['getTask']),
+    task () {
+      console.log(this.$route.params.id)
+      return this.getTask(this.$route.params.id)
+    }
+  }
+}
+</script>
+
+<style>
+.task-view {
+  @apply relative flex flex-row bg-white pin mx-4 m-32 mx-auto py-4 text-left rounded shadow;
+  max-width: 700px;
+}
+</style>
